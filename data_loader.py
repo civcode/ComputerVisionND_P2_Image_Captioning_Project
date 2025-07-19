3781e6e7d2401900a4f45cf9f9ee024bce7af19f
chris <ivi3001@yahoo.de>
2025-07-19 20:08:15 +0200

    merged changes from cloud workspace

--- data_loader.py
+++ data_loader.py
@@ -10,95 +10,25 @@ from tqdm import tqdm
 import random
 import json
 
-def get_loader(transform,
-               mode='train',
-               batch_size=1,
-               vocab_threshold=None,
-               vocab_file='./vocab.pkl',
-               start_word="<start>",
-               end_word="<end>",
-               unk_word="<unk>",
-               vocab_from_file=True,
-               num_workers=0,
-               cocoapi_loc='/opt'):
-    """Returns the data loader.
-    Args:
-      transform: Image transform.
-      mode: One of 'train' or 'test'.
-      batch_size: Batch size (if in testing mode, must have batch_size=1).
-      vocab_threshold: Minimum word count threshold.
-      vocab_file: File containing the vocabulary. 
-      start_word: Special word denoting sentence start.
-      end_word: Special word denoting sentence end.
-      unk_word: Special word denoting unknown words.
-      vocab_from_file: If False, create vocab from scratch & override any existing vocab_file.
-                       If True, load vocab from from existing vocab_file, if it exists.
-      num_workers: Number of subprocesses to use for data loading 
-      cocoapi_loc: The location of the folder containing the COCO API: https://github.com/cocodataset/cocoapi
-    """
-    
-    assert mode in ['train', 'test'], "mode must be one of 'train' or 'test'."
-    if vocab_from_file==False: assert mode=='train', "To generate vocab from captions file, must be in training mode (mode='train')."
-
-    # Based on mode (train, val, test), obtain img_folder and annotations_file.
-    if mode == 'train':
-        if vocab_from_file==True: assert os.path.exists(vocab_file), "vocab_file does not exist.  Change vocab_from_file to False to create vocab_file."
-        img_folder = os.path.join(cocoapi_loc, 'cocoapi/images/train2014/')
-        annotations_file = os.path.join(cocoapi_loc, 'cocoapi/annotations/captions_train2014.json')
-    if mode == 'test':
-        assert batch_size==1, "Please change batch_size to 1 if testing your model."
-        assert os.path.exists(vocab_file), "Must first generate vocab.pkl from training data."
-        assert vocab_from_file==True, "Change vocab_from_file to True."
-        img_folder = os.path.join(cocoapi_loc, 'cocoapi/images/test2014/')
-        annotations_file = os.path.join(cocoapi_loc, 'cocoapi/annotations/image_info_test2014.json')
-
-    # COCO caption dataset.
-    dataset = CoCoDataset(transform=transform,
-                          mode=mode,
-                          batch_size=batch_size,
-                          vocab_threshold=vocab_threshold,
-                          vocab_file=vocab_file,
-                          start_word=start_word,
-                          end_word=end_word,
-                          unk_word=unk_word,
-                          annotations_file=annotations_file,
-                          vocab_from_file=vocab_from_file,
-                          img_folder=img_folder)
-
-    if mode == 'train':
-        # Randomly sample a caption length, and sample indices with that length.
-        indices = dataset.get_train_indices()
-        # Create and assign a batch sampler to retrieve a batch with the sampled indices.
-        initial_sampler = data.sampler.SubsetRandomSampler(indices=indices)
-        # data loader for COCO dataset.
-        data_loader = data.DataLoader(dataset=dataset, 
-                                      num_workers=num_workers,
-                                      batch_sampler=data.sampler.BatchSampler(sampler=initial_sampler,
-                                                                              batch_size=dataset.batch_size,
-                                                                              drop_last=False))
-    else:
-        data_loader = data.DataLoader(dataset=dataset,
-                                      batch_size=dataset.batch_size,
-                                      shuffle=True,
-                                      num_workers=num_workers)
-
-    return data_loader
-
 class CoCoDataset(data.Dataset):
-    
-    def __init__(self, transform, mode, batch_size, vocab_threshold, vocab_file, start_word, 
-        end_word, unk_word, annotations_file, vocab_from_file, img_folder):
+    def __init__(self, transform, mode, batch_size, vocab_threshold, vocab_file, start_word, end_word, unk_word, annotations_file, vocab_from_file, img_folder, subset_size=None):
         self.transform = transform
         self.mode = mode
         self.batch_size = batch_size
         self.vocab = Vocabulary(vocab_threshold, vocab_file, start_word,
-            end_word, unk_word, annotations_file, vocab_from_file)
+                                end_word, unk_word, annotations_file, vocab_from_file)
         self.img_folder = img_folder
+
         if self.mode == 'train':
             self.coco = COCO(annotations_file)
             self.ids = list(self.coco.anns.keys())
-            print('Obtaining caption lengths...')
-            all_tokens = [nltk.tokenize.word_tokenize(str(self.coco.anns[self.ids[index]]['caption']).lower()) for index in tqdm(np.arange(len(self.ids)))]
+
+            # Randomly select a subset if subset_size is specified
+            if subset_size is not None and subset_size < len(self.ids):
+                self.ids = random.sample(self.ids, subset_size)
+
+            print('Obtaining caption lengths for the subset...')
+            all_tokens = [nltk.tokenize.word_tokenize(str(self.coco.anns[id]['caption']).lower()) for id in tqdm(self.ids)]
             self.caption_lengths = [len(token) for token in all_tokens]
         else:
             test_info = json.loads(open(annotations_file).read())
@@ -141,7 +71,7 @@ class CoCoDataset(data.Dataset):
 
     def get_train_indices(self):
         sel_length = np.random.choice(self.caption_lengths)
-        all_indices = np.where([self.caption_lengths[i] == sel_length for i in np.arange(len(self.caption_lengths))])[0]
+        all_indices = [i for i, c in enumerate(self.caption_lengths) if c == sel_length]
         indices = list(np.random.choice(all_indices, size=self.batch_size))
         return indices
 
@@ -150,3 +80,57 @@ class CoCoDataset(data.Dataset):
             return len(self.ids)
         else:
             return len(self.paths)
+        
+def get_loader(transform, mode='train', 
+               batch_size=1, 
+               vocab_threshold=None, 
+               vocab_file='./vocab.pkl', 
+               start_word="<start>", 
+               end_word="<end>", 
+               unk_word="<unk>", 
+               vocab_from_file=True, 
+               num_workers=0, 
+               cocoapi_loc='/opt', 
+               subset_size=None):
+    
+    assert mode in ['train', 'test'], "mode must be one of 'train' or 'test'."
+
+    if mode == 'train':
+        if vocab_from_file: 
+            assert os.path.exists(vocab_file), "vocab_file does not exist. Change vocab_from_file to False to create vocab_file."
+        img_folder = os.path.join(cocoapi_loc, 'cocoapi/images/train2014/')
+        annotations_file = os.path.join(cocoapi_loc, 'cocoapi/annotations/captions_train2014.json')
+    elif mode == 'test':
+        assert batch_size == 1, "Please change batch_size to 1 if testing your model."
+        assert os.path.exists(vocab_file), "Must first generate vocab.pkl from training data."
+        img_folder = os.path.join(cocoapi_loc, 'cocoapi/images/test2014/')
+        annotations_file = os.path.join(cocoapi_loc, 'cocoapi/annotations/image_info_test2014.json')
+
+    dataset = CoCoDataset(transform=transform, 
+                          mode=mode, 
+                          batch_size=batch_size, 
+                          vocab_threshold=vocab_threshold, 
+                          vocab_file=vocab_file, 
+                          start_word=start_word, 
+                          end_word=end_word, 
+                          unk_word=unk_word, 
+                          annotations_file=annotations_file, 
+                          vocab_from_file=vocab_from_file, 
+                          img_folder=img_folder, 
+                          subset_size=subset_size)
+
+    if mode == 'train':
+        indices = dataset.get_train_indices()
+        initial_sampler = data.sampler.SubsetRandomSampler(indices=indices)
+        data_loader = data.DataLoader(dataset=dataset, 
+                                      num_workers=num_workers,
+                                      batch_sampler=data.sampler.BatchSampler(sampler=initial_sampler,
+                                                                              batch_size=dataset.batch_size,
+                                                                              drop_last=False))
+    else:
+        data_loader = data.DataLoader(dataset=dataset, 
+                                      batch_size=dataset.batch_size,
+                                      shuffle=True, 
+                                      num_workers=num_workers)
+
+    return data_loader
--- model.py
+++ model.py
@@ -2,32 +2,49 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
-
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
         super(EncoderCNN, self).__init__()
-        resnet = models.resnet50(pretrained=True)
+        # Using ResNet-34 for a lighter model
+        resnet = models.resnet18(pretrained=True)
         for param in resnet.parameters():
             param.requires_grad_(False)
         
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
+        self.batch_norm = nn.BatchNorm1d(resnet.fc.in_features)
         self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+        
 
     def forward(self, images):
         features = self.resnet(images)
         features = features.view(features.size(0), -1)
+        features = self.batch_norm(features)
         features = self.embed(features)
         return features
-    
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.gru = nn.GRU(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+
     def forward(self, features, captions):
-        pass
+        embeddings = self.embed(captions[:, :-1])  # Exclude the <end> token
+        features = features.unsqueeze(1)
+        inputs = torch.cat((features, embeddings), dim=1) 
+        hiddens, _ = self.gru(inputs)
+        outputs = self.linear(hiddens) 
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        "accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len)"
+        predicted_sentence = []
+        for i in range(max_len):
+            hiddens, states = self.gru(inputs, states)
+            outputs = self.linear(hiddens.squeeze(1))
+            _, predicted = outputs.max(1)
+            predicted_sentence.append(predicted.item())
+            inputs = self.embed(predicted).unsqueeze(1)
+        return predicted_sentence
