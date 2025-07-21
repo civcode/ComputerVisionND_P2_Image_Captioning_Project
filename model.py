3350a5178ced3243b6fc4445774504094d8cc8f0
chris <ivi3001@yahoo.de>
2025-07-21 11:42:30 +0200

    backup

--- model.py
+++ model.py
@@ -9,11 +9,14 @@ class EncoderCNN(nn.Module):
         resnet = models.resnet18(pretrained=True)
         for param in resnet.parameters():
             param.requires_grad_(False)
+        #for param in resnet.layer4.parameters():
+        #    param.requires_grad_(True)
         
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
         self.batch_norm = nn.BatchNorm1d(resnet.fc.in_features)
         self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+        self.drop = nn.Dropout(p=0.3)
         
 
     def forward(self, images):
@@ -21,20 +24,24 @@ class EncoderCNN(nn.Module):
         features = features.view(features.size(0), -1)
         features = self.batch_norm(features)
         features = self.embed(features)
+        features = self.drop(features)
         return features
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
         super(DecoderRNN, self).__init__()
         self.embed = nn.Embedding(vocab_size, embed_size)
-        self.gru = nn.GRU(embed_size, hidden_size, num_layers, batch_first=True)
+        self.gru = nn.GRU(embed_size, hidden_size, num_layers, batch_first=True, dropout=0.3 if num_layers > 1 else 0)
         self.linear = nn.Linear(hidden_size, vocab_size)
+        self.drop = nn.Dropout(p=0.3)
 
     def forward(self, features, captions):
         embeddings = self.embed(captions[:, :-1])  # Exclude the <end> token
+        embeddings = self.drop(embeddings)
         features = features.unsqueeze(1)
         inputs = torch.cat((features, embeddings), dim=1) 
         hiddens, _ = self.gru(inputs)
+        hiddens = self.drop(hiddens)
         outputs = self.linear(hiddens) 
         return outputs
 
