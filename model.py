104d2ab711f5d9f7ad6dec753302f120bc75b5b3
chris <ivi3001@yahoo.de>
2025-07-21 14:54:56 +0200

    fixed bugs in train loop

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ class EncoderCNN(nn.Module):
         super(EncoderCNN, self).__init__()
         # Using ResNet-34 for a lighter model
         resnet = models.resnet18(pretrained=True)
+        #resnet = models.resnet34(pretrained=True)
         for param in resnet.parameters():
             param.requires_grad_(False)
         #for param in resnet.layer4.parameters():
@@ -16,7 +17,7 @@ class EncoderCNN(nn.Module):
         self.resnet = nn.Sequential(*modules)
         self.batch_norm = nn.BatchNorm1d(resnet.fc.in_features)
         self.embed = nn.Linear(resnet.fc.in_features, embed_size)
-        self.drop = nn.Dropout(p=0.3)
+        self.drop = nn.Dropout(p=0.0)
         
 
     def forward(self, images):
@@ -24,16 +25,17 @@ class EncoderCNN(nn.Module):
         features = features.view(features.size(0), -1)
         features = self.batch_norm(features)
         features = self.embed(features)
-        features = self.drop(features)
+        #features = self.drop(features)
         return features
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
         super(DecoderRNN, self).__init__()
         self.embed = nn.Embedding(vocab_size, embed_size)
-        self.gru = nn.GRU(embed_size, hidden_size, num_layers, batch_first=True, dropout=0.3 if num_layers > 1 else 0)
+        self.gru = nn.GRU(embed_size, hidden_size, num_layers)
+        #self.gru = nn.GRU(embed_size, hidden_size, num_layers, batch_first=True, dropout=0.0 if num_layers > 1 else 0)
         self.linear = nn.Linear(hidden_size, vocab_size)
-        self.drop = nn.Dropout(p=0.3)
+        self.drop = nn.Dropout(p=0.0)
 
     def forward(self, features, captions):
         embeddings = self.embed(captions[:, :-1])  # Exclude the <end> token
@@ -41,7 +43,7 @@ class DecoderRNN(nn.Module):
         features = features.unsqueeze(1)
         inputs = torch.cat((features, embeddings), dim=1) 
         hiddens, _ = self.gru(inputs)
-        hiddens = self.drop(hiddens)
+        #hiddens = self.drop(hiddens)
         outputs = self.linear(hiddens) 
         return outputs
 
