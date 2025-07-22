cd979e8d862c678b066f5ca8752a04fccc71cc5f
chris <ivi3001@yahoo.de>
2025-07-22 12:13:14 +0200

    added captions generattion for validation

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
         super(EncoderCNN, self).__init__()
         # Using ResNet-34 for a lighter model
-        resnet = models.resnet18(pretrained=True)
+        resnet = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
         for param in resnet.parameters():
             param.requires_grad_(False)
         
@@ -27,14 +27,15 @@ class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
         super(DecoderRNN, self).__init__()
         self.embed = nn.Embedding(vocab_size, embed_size)
-        self.gru = nn.GRU(embed_size, hidden_size, num_layers, batch_first=True)
+        self.rnn = nn.GRU(embed_size, hidden_size, num_layers, batch_first=True)
+        #self.rnn = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
         self.linear = nn.Linear(hidden_size, vocab_size)
 
     def forward(self, features, captions):
         embeddings = self.embed(captions[:, :-1])  # Exclude the <end> token
         features = features.unsqueeze(1)
         inputs = torch.cat((features, embeddings), dim=1) 
-        hiddens, _ = self.gru(inputs)
+        hiddens, _ = self.rnn(inputs)
         outputs = self.linear(hiddens) 
         return outputs
 
@@ -42,7 +43,7 @@ class DecoderRNN(nn.Module):
         "accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len)"
         predicted_sentence = []
         for i in range(max_len):
-            hiddens, states = self.gru(inputs, states)
+            hiddens, states = self.rnn(inputs, states)
             outputs = self.linear(hiddens.squeeze(1))
             _, predicted = outputs.max(1)
             predicted_sentence.append(predicted.item())
